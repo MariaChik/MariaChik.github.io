ef2f0a5532e5959c2568f2e18bf9bccad7571aad
MariaChik <131902195+MariaChik@users.noreply.github.com>
2023-04-27 00:25:32 +0300

    Add files via upload

---
+++ js-en.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Reaching a goal does not make a person great, but the journey to it.')
+})
---
+++ js-es.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('El hecho de alcanzar una meta no hace grande a una persona, sino el camino hacia ella.')
+})
